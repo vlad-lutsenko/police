c95a0fd0881c67e4fff1c3285e7c32313a374a58
Vlad Lutsenko <lutsenko.vladyslav@gmail.com>
2020-09-10 23:34:42 +0300

    axios.defaults.headers.common[Access-Control-Allow-Origin] = *;

--- src/helpers/fetch.js
+++ src/helpers/fetch.js
@@ -2,7 +2,9 @@ import axios from "axios";
 
 // axios.defaults.baseURL = "http://localhost:3000";
 axios.defaults.baseURL = "https://policememorial.herokuapp.com";
-// axios.defaults.baseURL = "https://git.heroku.com/policememorial.git";
+axios.defaults.headers.common["Access-Control-Allow-Origin"] = "*";
+// axios.defaults.headers.common["Access-Control-Allow-Headers"] =
+//   "Origin, X-Requested-With, Content-Type, Accept";
 
 export const getList = async () => await axios.get("/last-duty");
 
